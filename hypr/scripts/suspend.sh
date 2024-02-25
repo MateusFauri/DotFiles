ce2d287ef5ba711feb58bc19e1116089f10957e7
Mateus fauri <faurimateus@gmail.com>
2024-02-25 18:09:07 -0300

    New settup

--- hypr/scripts/suspend.sh
+++ hypr/scripts/suspend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 swayidle -w \
-timeout 120 ' swaylock ' \
-timeout 400 ' hyprctl dispatch dpms off' \
-timeout 12000 'systemctl suspend' \
+timeout 120000 ' swaylock ' \
+timeout 400000 ' hyprctl dispatch dpms off' \
+timeout 12000000 'systemctl suspend' \
 resume ' hyprctl dispatch dpms on' \
 before-sleep 'swaylock'
